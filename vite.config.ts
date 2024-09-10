c1b5b566cb2fb4714e7ad8e603ed887ea9de9b8f
Dorian <dorianpernot2@gmail.com>
2024-09-11 00:27:21 +0200

    second

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Modal-Documentation/',
+  base: 'Presentation_P14/',
 })
